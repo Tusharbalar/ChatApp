ee923ffbe0e4e3b0196925212da38074414ae157
Tushar Balar <balar.tushar1@gmail.com>
2017-09-04 21:44:45 +0530

    Tabs improved

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TabsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { IonicPage } from 'ionic-angular';
 
 @IonicPage()
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
+
 export class TabsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  tab1: string = "ChatsPage";
+  tab2: string = "GroupsPage";
+  tab3: string = "ProfilePage";
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
-  }
 
 }
