deea623bc768bdcad1c1cb6e404e6ed624b718d9
Tushar Balar <balar.tushar1@gmail.com>
2017-09-08 00:17:11 +0530

    Chats page updated Request sent successfully

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, Events } from 'ionic-angular';
+import { AppProvider } from "../../providers/common";
+import { RequestsProvider } from "../../providers/requests/requests";
 
 @IonicPage()
 @Component({
@@ -8,7 +10,20 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class ChatsPage {
 
-  constructor(public navCtrl: NavController) {
+  myrequests = [];
+
+  constructor(public navCtrl: NavController,
+              private appService: AppProvider,
+              private events: Events,
+              private requestService: RequestsProvider) {
+  }
+
+  ionViewWillEnter() {
+    this.requestService.getMyRequests();
+    this.events.subscribe('gotrequests', () => {
+      this.myrequests = [];
+      this.myrequests = this.requestService.userDetails;
+    })
   }
 
   addBuddy() {
--- src/providers/requests/requests.ts
+++ src/providers/requests/requests.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import firebase from 'firebase';
 import { connreq } from "../../models/interfaces/usercreds";
+import { UserProvider } from "../user/user";
+import { Events } from "ionic-angular";
 
 @Injectable()
 export class RequestsProvider {
 
   firereq = firebase.database().ref('/requests');
+  userDetails = [];
 
-  constructor() {
+  constructor(private userService: UserProvider,
+              private events: Events) {
   }
 
   sendRequest(req: connreq) {
@@ -21,6 +25,32 @@ export class RequestsProvider {
       });
     });
     return promise;  
+  }
+
+  getMyRequests() {
+    let myRequests = [];
+    let allRequests;
+    this.firereq.child(firebase.auth().currentUser.uid).on("value", (snapshot) => {
+      allRequests = snapshot.val();
+      myRequests = [];
+      // if (myRequests.length == 0) { return; }
+      for (var i in allRequests) {
+        console.log("sdsdas", myRequests, allRequests[i])
+        myRequests.push(allRequests[i].sender);
+      }
+      this.userService.getAllUsers().then((users) => {
+        let allUsers: any = users;
+        myRequests.forEach((request, index) => {
+          allUsers.forEach((user, index2) => {
+            console.log("2", myRequests[index], allUsers[index2].uid)
+            if (myRequests[index] === allUsers[index2].uid) {
+              this.userDetails.push(allUsers[index2]);
+            }
+          });
+        });
+        this.events.publish("gotrequests");
+      });
+    });
   }
 
 }
