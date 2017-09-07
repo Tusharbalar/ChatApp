875de4e26e277e0be69ecf3b5fa67194598bdcf1
Tushar Balar <balar.tushar1@gmail.com>
2017-09-07 21:18:17 +0530

    Implement user search functions

--- src/pages/buddies/buddies.ts
+++ src/pages/buddies/buddies.ts
@@ -11,15 +11,31 @@ import { UserProvider } from "../../providers/user/user";
 export class BuddiesPage {
 
   users: any;
+  tmparr: any;
 
   constructor(public navCtrl: NavController,
-              public userService: UserProvider) {
+    public userService: UserProvider) {
   }
 
   ionViewWillEnter() {
     this.userService.getAllUsers().then((res) => {
       this.users = res;
+      this.tmparr = res;
     });
+  }
+
+  searchBuddy(ev: any) {
+    this.users = this.tmparr;
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.users = this.users.filter((item) => {
+        return (item.displayName.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    }
   }
 
 }
